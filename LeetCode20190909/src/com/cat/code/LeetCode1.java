38ec337a2d952593bae9f53b63e24da7938cc218
2637977081 <2637977081@qq.com>
2019-09-09 22:52:46 +0800

    LeetCode第一题提交 使用Map映射 时间复杂度O(n) 空间复杂度较高
    gitignore 忽略Java工程输出的class文件

---
+++ LeetCode20190909/src/com/cat/code/LeetCode1.java
@@ -0,0 +1,41 @@
+package com.cat.code;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LeetCode1 {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        Map<Integer,String> map = new HashMap<>();
+        //将所有数值存入map中
+        for(int i=0;i<nums.length;i++){
+            String value = i+",";
+            if(map.get(nums[i])!=null){
+                value = map.get(nums[i])+value;
+            }
+            map.put(nums[i],value);
+        }
+        //现在找到该数字
+        for(int i=0;i<nums.length;i++){
+            int temp = target - nums[i];
+            String value = map.get(nums[i]);
+            value = value.substring((""+i).length()+1,value.length());
+            map.put(nums[i],value);
+
+            String tempString = map.get(temp);
+            if(tempString!=null&&!tempString.equals("")){
+                result[0] = i;
+                result[1] =Integer.valueOf(tempString.substring(0,tempString.indexOf(",")));
+                break;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        LeetCode1 leetCode1 = new LeetCode1();
+        int[] result =leetCode1.twoSum(new int[]{3, 3, 3, 15},6);
+        System.out.println(result[0]+"---"+result[1]);
+    }
+}
