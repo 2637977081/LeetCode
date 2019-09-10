ad1cfb9bafdf356846c05c180d2748e9cef80256
lvgang <lvgang@golaxy.cn>
2019-09-10 18:36:13 +0800

    LeetCode2

--- LeetCode20190909/src/com/cat/code/code2/LeetCode2.java
+++ LeetCode20190909/src/com/cat/code/code2/LeetCode2.java
@@ -6,25 +6,65 @@ public class LeetCode2 {
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode resListNode = new ListNode(-1);
+        ListNode newL1 = reverseList1(l1);
+        ListNode newL2 = reverseList1(l2);
+        while (newL1!=null||newL2!=null){
 
+        }
         return resListNode;
+    }
+
+    //循环倒序
+    public ListNode reverseList1(ListNode listNode){
+        if(listNode==null){
+            return null;
+        }
+        ListNode curNode = listNode;//当前节点
+        ListNode oldNode = null;//
+        ListNode newNode = null;
+        while (curNode!=null){
+            oldNode = curNode.next;
+            curNode.next = newNode;
+            newNode = curNode;
+            curNode = oldNode;
+        }
+        return newNode;
     }
 
 
 
-    public ListNode reverseList(ListNode listNode){
+    //递归倒序
+    public ListNode reverseList2(ListNode listNode){
         if(listNode==null || listNode.next==null){
             return listNode;
         }
         //保留最后两个节点，将最后一个节点传递给nowNode
-        ListNode nowNode = reverseList(listNode.next);
-        System.out.println("nowNode1:"+nowNode);
+        ListNode nowNode = reverseList2(listNode.next);
         //重复制造最后两个节点
         listNode.next.next = listNode;
-        System.out.println("nowNode2:"+nowNode);
+        //剪除下一节点
         listNode.next = null;
-        System.out.println("nowNode3:"+nowNode);
         return nowNode;
+    }
+
+    public String getStringFromListNode(ListNode listNode){
+        StringBuffer str=new StringBuffer("");
+        while (listNode!=null){
+            str.append(listNode.val);
+            listNode=listNode.next;
+        }
+        return str.toString();
+    }
+
+    public ListNode getListNodeFromString(String str){
+        ListNode listNode =null;
+        int index =0;
+        while (index<str.length()){
+            listNode.val=Integer.valueOf(str.substring(index,index+1));
+            listNode.next = listNode;
+            index++;
+        }
+        return listNode;
     }
 
     public static void main(String[] args){
@@ -34,8 +74,6 @@ public class LeetCode2 {
         l1.next=new ListNode(4);
         l1.next.next = new ListNode(3);
         l1.next.next.next = new ListNode(5);
-        System.out.println(l1);
-        System.out.println(leetCode.reverseList(l1));
 
         ListNode l2 = new ListNode(5);
         l2.next=new ListNode(6);
